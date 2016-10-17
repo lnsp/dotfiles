b29cc951a8a984de3df5761b780f8fda9ab7f5ec
Lennart Espe <lennart@mooxmirror.io>
2016-10-17 23:25:04 +0200

    Update

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,17 @@
-#!/usr/bin/bash
+#!/bin/bash
 
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 # bootstrap homebrew
+echo "Installing homebrew ..."
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+echo "Installing command line tools ..."
 ./brew.sh
 
 # bootstrap oh my zsh
+echo "Installing oh-my-zsh ..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # run .macos config
+echo "Configuring macOS ..."
 ./macos.sh
+echo "Done."
