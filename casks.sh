cb70e5549f155b8fe3f4185e197b89a042ace27b
Lennart Espe <lnsp@users.noreply.github.com>
2019-08-01 14:52:21 +0200

    Add casks

--- casks.sh
+++ casks.sh
@@ -2,10 +2,33 @@
 
 # install apps
 brew tap caskroom/cask
-brew cask install hyper
 brew cask install google-chrome
-brew cask install atom
 brew cask install transmission
-brew cask install sizeup
-brew cask install iterm2
+brew cask install twitch
+brew cask install texpad
+brew cask install vscode
+brew cask install vlc
+brew cask install minecraft
+brew cask install telegram
+brew cask install spotify
+brew cask install keybase
+brew cask install docker
+brew cask install minikube
+brew cask install spectacle
+brew cask install intellij-idea
+brew cask install mactex-no-gui
+brew cask install insomnia
+brew cask install typora
+brew cask install firefox
+brew cask install datagrip
+brew cask install mediathekview
+brew cask install keepassxc
+brew cask install grandperspective
+brew cask install atom
+brew cask install goland
+brew cask install macs-fan-control
+brew cask install balenaetcher
+brew cask install tunnelblick
+brew cask install tor-browser
+brew cask install skype
 
